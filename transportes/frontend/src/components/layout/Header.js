593f179ab55ae3f79289595eab5bd1fd6e3b5b65
enzoreinozo <ereinozo1986@gmail.com>
2024-11-08 11:44:09 -0300

    transportes react created

---
+++ transportes/frontend/src/components/layout/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Header = (props) => {
+    return (
+        <header>
+        <div className="holder">
+        <img src="img/transporte.jpg" witdth="100" alt="Transportes X" />
+        <h1>Transportes X</h1>
+        </div>
+        </header>
+    )
+}
+export default Header;
