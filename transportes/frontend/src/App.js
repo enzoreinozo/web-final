5880391256027e35392e8e0356ac949611049979
enzoreinozo <ereinozo1986@gmail.com>
2024-11-27 18:40:47 -0300

    react router dom installed

--- transportes/frontend/src/App.js
+++ transportes/frontend/src/App.js
@@ -33,5 +33,22 @@ function App() {
     </div>
   );
 }
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
+
 
 export default App;
